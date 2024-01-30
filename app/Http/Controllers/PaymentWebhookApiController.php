3b4aff3905471f7b1d6f61cba4e7f367a1ac6c20
oketafred <oketafred@gmail.com>
2024-01-30 14:52:19 +0300

    Implement relworx webhook

--- app/Http/Controllers/PaymentWebhookApiController.php
+++ app/Http/Controllers/PaymentWebhookApiController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\SmsBundle;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class PaymentWebhookApiController extends Controller
 {
+    public function webhook(Request $request)
+    {
+        try {
+            $payload = $request->all();
 
+            $smsBundle = SmsBundle::query()
+                ->where('external_id', $payload['internal_reference'])
+                ->where('transaction_reference', $payload['customer_reference'])
+                ->where('amount', $payload['amount'])
+                ->where('status', SmsBundle::STATUS_PENDING)
+                ->where('phone_number', $payload['msisdn'])
+                ->first();
+
+            if (!$smsBundle) {
+                throw new Exception('SmsBundle not found for payment webhook');
+            }
+
+            $smsBundle->status = $payload['status'];
+            $smsBundle->payment_type = $payload['provider'];
+            $smsBundle->transaction_fee = $payload['charge'];
+            $smsBundle->save();
+
+            return response()->json([
+                'message' => 'Transaction successful'
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            Log::error('Error in payment webhook: ' . $exception->getMessage());
+
+            return response()->json([
+                'message' => 'Error occurred transaction could not be completed!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/webhook/flutterwave', [PaymentWebhookApiController::class, 'webhook'])
+Route::post('/webhook/relworx', [PaymentWebhookApiController::class, 'webhook'])
     ->name('webhook');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
